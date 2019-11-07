50ba5670b2ebeb57bf0e80968ac5e05da122d646
David Goldberg <dgoldberg@dropbox.com>
2019-11-06 16:20:23 -0800

    corrected test file bugs

--- test/Spec.hs
+++ test/Spec.hs
@@ -11,42 +11,44 @@ import qualified Challenge6 as C6
 challenge1Test = TestCase 
                $ assertEqual 
                     "Challenge 1: Convert hex to base64" 
-                    C1.challenge1
                     C1.base64Output 
+                    C1.challenge1
 
 challenge2Test = TestCase 
                $ assertEqual 
                     "Challenge 2: Fixed XOR"
-                    C2.challenge2 
                     C2.expectedCipherText
+                    C2.challenge2 
 
 challenge3Test = TestCase
-                $ assertEqual "Single-byte XOR cipher" C3.challenge3 'X'
+                $ assertEqual "Single-byte XOR cipher" 'X' C3.challenge3 
 
 challenge4Test = TestCase 
                $ do 
                    c4Result <- C4.challenge4
-                   assertEqual "Detect single-character XOR" c4Result '5'
+                   assertEqual "Detect single-character XOR" '5' c4Result 
                     
 challenge5Test = TestCase 
                $ assertEqual 
                     "Challenge 5: Implement repeating-key XOR"
-                    C5.challenge5
                     C5.expectedCipherText
+                    C5.challenge5
 
-chalenge6Test = TestCase
+challenge6Test = TestCase
               $ do 
                   result <- C6.challenge6
                   assertEqual
                       "Challenge 6: Break repeating-key XOR"
+                      ["I'm", "back"]
                       result
-                      ["I'm", "Back"]
                     
 
 testList = TestList [ challenge1Test
                     , challenge2Test
                     , challenge3Test
                     , challenge4Test
+                    , challenge5Test
+                    , challenge6Test
                     ]
 main :: IO ()
 main = do
