a5d8bdf313c67b7d81279b0ac7f2c79e8fb6e6d7
David Goldberg <dgoldberg@dropbox.com>
2019-11-06 21:30:23 -0800

    a little more work on challenge 12

--- src/challenge12.hs
+++ src/challenge12.hs
@@ -5,6 +5,11 @@ import qualified Data.ByteString as B
 import BlockOracle
 import qualified Lib
 
+type Oracle = B.ByteString -> IO B.ByteString
+
+mostCommon :: Ord a => [a] -> a
+mostCommon = snd . maximum . map (\xs -> (length xs, head xs)) . group . sort
+
 cipherText :: B.ByteString
 cipherText = Lib.base64ToBytes . Lib.stringToBytes $ "\
 \Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkg\
@@ -12,25 +17,42 @@ cipherText = Lib.base64ToBytes . Lib.stringToBytes $ "\
 \dXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUg\
 \YnkK"
 
-constructPlainText :: B.ByteString -> B.ByteString
-constructPlainText knownPlainText = B.append knownPlainText cipherText
+appendCipherText :: B.ByteString -> B.ByteString
+appendCipherText knownPlainText = B.append knownPlainText cipherText
 
-type Oracle = B.ByteString -> IO B.ByteString
+nBytePlainText :: Int -> B.ByteString
+nBytePlainText n = B.replicate n 'A'
 
-paddingOracle = getSimplePaddingOracle
+nBytePayload :: Int -> B.ByteString
+nBytePayload n = appendCipherText (nBytePlainText n)
 
-getNOracleOutputs 
+getNOracleOutputsHelper
     :: Oracle 
     -> B.ByteString 
     -> Int 
     -> [B.ByteString] 
     -> IO [B.ByteString]
-getNOracleOutputs oracle plainText nTrials acc
+getNOracleOutputsHelper oracle plainText nTrials acc
     | nTrials == 0 = return acc
     | otherwise = do
-        return getNOracleOutputs oracle 
-    
+        return getNOracleOutputs oracle (nTrials-1) plainText (result:acc)
+    where
+        result = oracle plainText
 
-detectBlockSize :: Oracle -> Int
-detectBlockSize oracle = 
+getNOracleOutputs
+    :: Oracle 
+    -> B.ByteString 
+    -> Int 
+    -> IO [B.ByteString]
+getNOracleOutputs oracle plainText nTrials = result
+    where result = getNOracleOutputs oracle plainText nTrials []
 
+mostCommonCipherTextLength :: Oracle -> B.ByteString -> IO Int
+mostCommonCipherTextLength oracle plainText = do
+    outputs <- getNOracleOutputs oracle plainText 20
+    return $ mostCommon results
+
+-- detectBlockSize :: Oracle -> Int
+-- detectBlockSize oracle = blockSize
+--     where
+       
