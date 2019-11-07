5b2c5799a5cc535d558d0277f78b2f91caa4ccf4
David Goldberg <dgoldberg@dropbox.com>
2019-11-06 17:28:27 -0800

    refactored to have blockOracle library file

---
+++ src/blockOracle.hs
@@ -0,0 +1,64 @@
+module BlockOracle( EncryptionMode(..)
+                  , modeOracleWithAnswer
+) where
+
+import qualified Data.ByteString as B
+import Data.Word(Word8)
+import System.Random
+
+import qualified Lib
+
+type BLetterStream = [Word8]
+data EncryptionMode = ECB | CBC deriving (Show, Eq)
+
+getRandomLetterStream :: StdGen -> (BLetterStream, StdGen)
+getRandomLetterStream g = (randomRs (0, 255) g, g)
+
+randomByteString :: BLetterStream -> Int -> (B.ByteString, BLetterStream)
+randomByteString rLetters n = (result, rest)
+    where result = B.pack these
+          (these, rest) = splitAt n rLetters
+
+coinFlip :: StdGen -> (Bool, StdGen)
+coinFlip g = random g
+
+randomPadLength :: StdGen -> (Int, StdGen)
+randomPadLength g = randomR (5,10) g
+
+getGarbledPlainText 
+    :: StdGen
+    -> B.ByteString 
+    -> (B.ByteString, StdGen)
+getGarbledPlainText g plainText = (garbled, g'')
+    where
+        garbled = B.concat [prefix, plainText, suffix]
+        (suffix, _) = randomByteString rest suffixLength
+        (prefix, rest) = randomByteString letterStream prefixLength
+
+        (letterStream, g''') = getRandomLetterStream g''
+        (prefixLength, g'') = randomPadLength g'
+        (suffixLength, g') = randomPadLength g
+
+getRandomAESKey :: StdGen -> (B.ByteString, StdGen)
+getRandomAESKey gen = (key, gen')
+    where 
+        (key, _) = randomByteString letterStream 16
+        (letterStream, gen') = getRandomLetterStream gen
+
+
+modeOracleWithAnswer :: StdGen -> B.ByteString -> (EncryptionMode, B.ByteString)
+modeOracleWithAnswer gen plainText = result
+    where
+        result = case coinFlipResult of
+                   True -> (ECB, Lib.ecbEncryption aes garbledPlainText)
+                   False -> (CBC, Lib.cbcEncryption aes iv garbledPlainText)
+        aes = Lib.initAES128 key
+        (key, gen') = getRandomAESKey gen
+        (iv, gen'') = getRandomAESKey gen'
+        (garbledPlainText, gen''') = getGarbledPlainText gen'' plainText
+        (coinFlipResult, _) = coinFlip gen'''
+
+
+-- simpleByteByByteOracle :: B.ByteString -> IO B.ByteString
+-- simpleByteByByteOracle plainText =
+--     do 
--- src/challenge11.hs
+++ src/challenge11.hs
@@ -1,63 +1,11 @@
 module Challenge11(challenge11) where
 
 import qualified Data.ByteString as B
-import qualified Data.ByteString.Char8 as BC
-import Data.Word(Word8)
-import Debug.Trace
 import System.Random
 
+import BlockOracle
 import qualified Lib
 
-type BLetterStream = [Word8]
-data EncryptionMode = ECB | CBC deriving (Show, Eq)
-
-getRandomLetterStream :: StdGen -> (BLetterStream, StdGen)
-getRandomLetterStream g = (randomRs (0, 255) g, g)
-
-randomByteString :: BLetterStream -> Int -> (B.ByteString, BLetterStream)
-randomByteString rLetters n = (result, rest)
-    where result = B.pack these
-          (these, rest) = splitAt n rLetters
-
-coinFlip :: StdGen -> (Bool, StdGen)
-coinFlip g = random g
-
-randomPadLength :: StdGen -> (Int, StdGen)
-randomPadLength g = randomR (5,10) g
-
-getGarbledPlainText 
-    :: StdGen
-    -> B.ByteString 
-    -> (B.ByteString, StdGen)
-getGarbledPlainText g plainText = (garbled, g'')
-    where
-        garbled = B.concat [prefix, plainText, suffix]
-        (suffix, _) = randomByteString rest suffixLength
-        (prefix, rest) = randomByteString letterStream prefixLength
-
-        (letterStream, g''') = getRandomLetterStream g''
-        (prefixLength, g'') = randomPadLength g'
-        (suffixLength, g') = randomPadLength g
-
-getRandomAESKey :: StdGen -> (B.ByteString, StdGen)
-getRandomAESKey gen = (key, gen')
-    where 
-        (key, _) = randomByteString letterStream 16
-        (letterStream, gen') = getRandomLetterStream gen
-        
-
-
-encryptionOracleWithAnswer :: StdGen -> B.ByteString -> (EncryptionMode, B.ByteString)
-encryptionOracleWithAnswer gen plainText = result
-    where
-        result = case coinFlipResult of
-                   True -> (ECB, Lib.ecbEncryption aes garbledPlainText)
-                   False -> (CBC, Lib.cbcEncryption aes iv garbledPlainText)
-        aes = Lib.initAES128 key
-        (key, gen') = getRandomAESKey gen
-        (iv, gen'') = getRandomAESKey gen'
-        (garbledPlainText, gen''') = getGarbledPlainText gen'' plainText
-        (coinFlipResult, _) = coinFlip gen'''
 
 guessEncryptionMode :: B.ByteString -> EncryptionMode
 guessEncryptionMode cipherText = if Lib.detectECB cipherText
@@ -82,7 +30,7 @@ detectionSuccessesForTrials :: Int -> Int -> IO Int
 detectionSuccessesForTrials 0 nSuccesses = return nSuccesses
 detectionSuccessesForTrials nTrials nSuccesses = do
     gen <- newStdGen
-    let (actualMode, cipherText) = encryptionOracleWithAnswer gen oracleInput
+    let (actualMode, cipherText) = modeOracleWithAnswer gen oracleInput
         guessedMode = guessEncryptionMode cipherText
     if guessedMode == actualMode
        then detectionSuccessesForTrials (nTrials-1) (nSuccesses+1)
