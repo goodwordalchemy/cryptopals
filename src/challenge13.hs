ec6414690ea1929179be348e200c0c9e31f5696a
David Goldberg <dgoldberg@dropbox.com>
2019-11-07 19:04:34 -0800

    made parser for url strings'

--- src/challenge13.hs
+++ src/challenge13.hs
@@ -1,1 +1,57 @@
-import Data.Lib
+import Text.ParserCombinators.ReadP
+import qualified Data.Map as Map
+import qualified Lib
+
+-- URL Parsing utilities --
+data UserProfile = UserProfile (Map.Map String String) deriving Show
+
+urlChars :: [Char]
+urlChars = ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9'] ++ "@%"
+
+pAmp :: ReadP Char
+pAmp = satisfy ('&' ==)
+
+pEquals :: ReadP Char
+pEquals = satisfy ('=' ==)
+
+
+pUrlChar :: ReadP Char
+pUrlChar = satisfy (\c -> c `elem` urlChars)
+
+
+pKey :: ReadP String
+pKey = do
+    key <- manyTill pUrlChar pEquals
+    return key
+    
+
+pValue :: ReadP String
+pValue = do
+    value <- (manyTill pUrlChar pAmp) <++ (manyTill pUrlChar eof)
+    return value
+
+pKV :: ReadP (String, String)
+pKV = do
+    key <- pKey
+    value <- pValue
+    return (key, value)
+
+profile :: ReadP UserProfile
+profile = do
+    kvs <- many1 pKV
+    return $ UserProfile $ Map.fromList kvs
+
+extractS :: ReadP a -> String -> a
+extractS p s = fst . head $ readP_to_S p s
+-- Tests --
+
+testParser :: IO ()
+testParser = do
+    -- let exampleUrl = "foo=bar&baz=qux&zap=zazzle"
+    let exampleUrl = "foo=bar"
+    -- putStrLn $ show $ readP_to_S profile exampleUrl
+    putStrLn $ show $ extractS profile exampleUrl
+
+main:: IO ()
+main = do
+    testParser
