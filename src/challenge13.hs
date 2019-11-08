e069d4724ed07043cf1a3221d1d41562d0d7c485
David Goldberg <dgoldberg@dropbox.com>
2019-11-07 20:08:00 -0800

    can create user profiles with clean emails

--- src/challenge13.hs
+++ src/challenge13.hs
@@ -1,12 +1,21 @@
 import Text.ParserCombinators.ReadP
-import qualified Data.Map as Map
+import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
+import Debug.Trace
 import qualified Lib
 
--- URL Parsing utilities --
 data UserProfile = UserProfile (Map.Map String String) deriving Show
 
-urlChars :: [Char]
-urlChars = ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9'] ++ "@%"
+extractS :: ReadP a -> String -> a
+-- extractS p s = head [x | (x,"") <- readP_to_S p s]
+extractS p s = fst . last $ readP_to_S p s
+
+lettersAndNumbers :: Set.Set Char
+lettersAndNumbers = Set.fromList $ ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9']
+
+-- URL Parsing utilities --
+urlChars :: Set.Set Char
+urlChars = lettersAndNumbers `Set.union` (Set.fromList "@%._")
 
 pAmp :: ReadP Char
 pAmp = satisfy ('&' ==)
@@ -16,18 +25,20 @@ pEquals = satisfy ('=' ==)
 
 
 pUrlChar :: ReadP Char
-pUrlChar = satisfy (\c -> c `elem` urlChars)
+pUrlChar = satisfy (\c -> c `Set.member` urlChars)
 
 
 pKey :: ReadP String
 pKey = do
-    key <- manyTill pUrlChar pEquals
+    key <- many1 pUrlChar
+    pEquals
     return key
     
 
 pValue :: ReadP String
 pValue = do
-    value <- (manyTill pUrlChar pAmp) <++ (manyTill pUrlChar eof)
+    value <- many1 pUrlChar
+    optional pAmp
     return value
 
 pKV :: ReadP (String, String)
@@ -41,17 +52,53 @@ profile = do
     kvs <- many1 pKV
     return $ UserProfile $ Map.fromList kvs
 
-extractS :: ReadP a -> String -> a
-extractS p s = fst . head $ readP_to_S p s
+urlParse :: String -> UserProfile
+urlParse url = extractS profile url
+
+-- urlUnparse :: UserProfile -> String
+-- urlUnparse = 
+    
+-- Email Parsing Utils --
+emailChars :: Set.Set Char
+emailChars = lettersAndNumbers `Set.union` (Set.fromList "@._")
+
+pEmailChar :: ReadP Char
+pEmailChar = satisfy (`Set.member` emailChars)
+
+pEmail :: ReadP String
+pEmail = do
+    validEmail <- many1 pEmailChar
+    return $ validEmail
+
+cleanEmail :: String -> String
+cleanEmail email = extractS pEmail email
+
+-- Oracle Utilities --
+profileFor :: String -> UserProfile
+profileFor email = UserProfile 
+                 $ Map.fromList [ ("email", clean)
+                                , ("role", "user")
+                                , ("uid", "10")
+                                ]
+    where clean = cleanEmail email
+
 -- Tests --
 
-testParser :: IO ()
-testParser = do
-    -- let exampleUrl = "foo=bar&baz=qux&zap=zazzle"
-    let exampleUrl = "foo=bar"
-    -- putStrLn $ show $ readP_to_S profile exampleUrl
+testUrlParser :: IO ()
+testUrlParser = do
+    let exampleUrl = "foo=bar&baz=qux&zap=zazzle"
     putStrLn $ show $ extractS profile exampleUrl
+
+testCleanEmail :: IO ()
+testCleanEmail = do
+    print $ cleanEmail "foo@bar.com&role=admin"
+
+testProfileFor :: IO ()
+testProfileFor = do
+    print $ profileFor "foo@bar.com&role=admin"
 
 main:: IO ()
 main = do
-    testParser
+    testUrlParser
+    testCleanEmail
+    testProfileFor
