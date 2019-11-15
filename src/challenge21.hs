0e6284d7ffd5d11d2291f8a3201e4add4f2c3d18
David Goldberg <dgoldberg@dropbox.com>
2019-11-15 14:07:37 -0800

    number generator matches python\!

--- src/challenge21.hs
+++ src/challenge21.hs
@@ -27,7 +27,7 @@ f = 1812433253
 ----------------
 
 lowestWBits :: Int -> Int
-lowestWBits x = x .&. (2^w-1)
+lowestWBits x = x .&. ((1 `shift` w) - 1)
 
 lowerMask = (1 `shift` r) - 1 
 upperMask = lowestWBits (complement lowerMask)
@@ -39,13 +39,12 @@ seedMtHelper idx stop prev
   | idx == stop = []
   | otherwise = thisValue : (seedMtHelper (idx+1) stop thisValue)
     where
-        thisValue = lowestWBits (f * (prev `xor` (prev `shift` (-w+2))) + idx)
-
+        thisValue = lowestWBits (f * (prev `xor` (prev `shiftR` (w-2))) + idx)
 
 seedMt :: Int -> MTState
-seedMt seed = (n, mtState)
+seedMt seed = (n, seed:mtState)
     where 
-        mtState = seedMtHelper 0 n seed
+        mtState = seedMtHelper 1 n seed
 
 twistHelper :: Int -> Int -> [Int] -> [Int]
 twistHelper idx stop prev
@@ -56,7 +55,7 @@ twistHelper idx stop prev
         thisValue = (prev !! ((idx + m) `mod` n)) `xor` xA'
 
         xA' = if (x `mod` 2) /= 0 then xA `xor` a else xA
-        xA = x `shift` (-1)
+        xA = x `shiftR` 1
 
         x = ((prev !! idx) .&. upperMask) 
           + ((prev !! ((idx+1) `mod` n)) .&. lowerMask)
@@ -65,14 +64,17 @@ twist :: MTState -> MTState
 twist (_, prev) = (0, twistHelper 0 n prev)
 
 extractNumber :: MTState -> (Int, MTState)
-extractNumber (idx, prev) = (r, (idx+1, cur))
+extractNumber (idx, prev) = (r, (idx'+1, cur))
     where
-        (idx', cur) = if idx >= n 
+        (idx', cur) = if idx == n 
                          then twist (idx, prev) 
                          else (idx, prev)
         y = cur !! idx'
         y' = y `xor` ((y `shiftR` u) .&. d)
         y'' = y' `xor` ((y' `shift` s) .&. b)
         y''' = y'' `xor` ((y'' `shift` t) .&. c)
-        r = y''' `shiftR` 1
+        y'''' = y''' `xor` (y''' `shiftR` l)
+        r = lowestWBits y''''
 
+-- challenge21 :: Bool
+-- challenge21 = 
