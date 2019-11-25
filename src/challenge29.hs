d979b23797a6baadc7184f86ded2810da793b6f9
David Goldberg <goodwordachemy@gmail.com>
2019-11-25 10:52:26 -0800

    padding appears to be correct

--- src/challenge29.hs
+++ src/challenge29.hs
@@ -7,16 +7,23 @@ import qualified Lib
 
 padLastBlock :: Int -> B.ByteString -> B.ByteString
 padLastBlock len block
-  | len `mod` 64 <= 56 = padded
-  | otherwise = block `B.append` (64 - (B.length block))
+  | len `mod` 64 < 56 = 
+    let zeros = B.replicate nZeros 0
+    in B.concat [ block'
+                , zeros
+                , encodedLength
+                ]
+
+  | otherwise = 
+      let zeros = B.replicate (64 + nZeros) 0
+      in B.concat [ block' 
+                  , zeros
+                  , encodedLength
+                  ]
     where
-        padded = B.concat [ block
-                          , B.singleton 0x80
-                          , zeros
-                          , encodedLength
-                          ]
-        zeros = B.replicate (55 - (len `mod` 64)) 0
+        nZeros =  55 - (len `mod` 64)
         encodedLength = Lib.bigEndian64 (8*len)
+        block' = block `B.append` (B.singleton 0x80)
 
 sha1PadHelper :: Int -> B.ByteString -> B.ByteString
 sha1PadHelper len text 
@@ -35,20 +42,19 @@ paddingIsSame text =
         myPadding = sha1Pad bs
         theirPadding = Lib.strictBL . padSHA1 . Lib.lazyB $ bs
     in 
-        (trace $ "myPadding:" 
-              ++ show myPadding 
-              ++ ", theirPadding: " 
-              ++ show theirPadding) $
     myPadding == theirPadding
     
 testSha1MdPad :: [(String, Bool)]
 testSha1MdPad =  Lib.mapWithOrig paddingIsSame 
     [ 
-    -- "hello"
-    -- , "This is a much longer string"
-    -- , replicate 600 'A'
-    replicate 56 'B'
-    -- , replicate 64 'C'
+    "hello"
+    , "This is a much longer string"
+    , replicate 600 'A'
+    , replicate 55 'D'
+    , replicate 56 'B'
+    , replicate 63 'C'
+    , replicate 64 'E'
+    , replicate 65 'F'
     ]
 
 main :: IO ()
