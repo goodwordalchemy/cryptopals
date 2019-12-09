ba19a6857ecd0e8b6a017aa7009643b8d2be24da
David Goldberg <goodwordachemy@gmail.com>
2019-12-09 05:53:16 -0800

    started challenge34

--- src/challenge34.hs
+++ src/challenge34.hs
@@ -3,13 +3,17 @@ import Data.ByteString as B
 type Message = B.ByteString
 
 data Person { name :: String
-            , keychain :: KeyChain
+            , privateKey :: String 
+            , keyChain :: Maybe KeyChain
             } deriving (Show)
 
-data KeyChain { privateKey :: String
-              , publicKey :: String
-              , base :: Int
-              , modulus :: Int
-              } deriving (Show)
+data KeyChain = {
+                , base :: Int
+                , modulus :: Int
+                , publicKey :: String
+                } deriving (Show)
 
 echoBot :: Bool
+echoBot aPriv bPriv = 
+    let a = Person "Alice" aPriv Nothing
+        b = Person "Bob" bPriv Nothing
